a4bf4ac487a5590ae699527c7014a09239a3f2df
JamesLukeD <jamesdaglessbsc@hotmail.com>
2025-07-28 22:47:22 +0100

    readding routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,21 @@ import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css"; // Optional global styles
 
 const App = () => {
   return (
-    <div className="App">
-      <Nav />
-      <main>
-        <Hero />
-        <Products />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <main>
+          <Hero />
+          <Products />
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
